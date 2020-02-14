f7a2d5e4f84c4848371cade17788237374b74e67
Dorian Weidler <dorian.weidler@novatec-gmbh.de>
2020-02-14 08:39:36 +0100

    Change

--- index.js
+++ index.js
@@ -16,11 +16,12 @@
  */
 
 var express = require('express');
+var os = require('os');
 
 var app = express();
 
 app.get('/', function (req, res) {
-  res.json({message: 'public'});
+  res.send(os.hostname())
 });
 
 app.listen(3000, function () {
